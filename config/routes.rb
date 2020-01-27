c49a751458106468f14f397bb9ad581a3ea1d39f
Franco <francoarriaza@hotmail.com>
2020-01-26 22:12:02 -0300

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   resources :histories
 
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
+
+  resources :sessions, only: [:create, :destroy]
+  
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
